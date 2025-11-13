c44a2d38671e874d6ad5619a3a50d4561098ee8c
bilaharivp <bilaharivp@gmail.com>
2025-11-13 15:34:25 +0530

    Changes from clone

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,14 +5,28 @@ function Contact() {
   return (
     <div className="page-container">
       <div className="page-content">
-      <h1>Contact Me</h1>
-      <div className="contact-info">
-        <p>Email: <strong>bilaharivp@example.com</strong></p>
-        <p>Phone: <strong>+91 7907452589</strong></p>
-        <p>LinkedIn: <a href="#" target="_blank">linkedin.com/in/bilaharivp</a></p>
-        <p>GitHub: <a href="#" target="_blank">github.com/bilaharivp</a></p>
+        <h1>Contact Me</h1>
+        <div className="contact-info">
+          <p>
+            Email: <strong>bilaharivp@gmail.com</strong>
+          </p>
+          <p>
+            Phone: <strong>+91 7907452589</strong>
+          </p>
+          <p>
+            LinkedIn:{" "}
+            <a href="#" target="_blank">
+              linkedin.com/in/bilaharivp
+            </a>
+          </p>
+          <p>
+            GitHub:{" "}
+            <a href="#" target="_blank">
+              github.com/bilaharivp
+            </a>
+          </p>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
